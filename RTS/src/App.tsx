b5588a023445460c887308a1e9e96bb7ebc08a24
Caique-FLopes <caiqueeflopes@hotmail.com>
2025-11-28 17:50:36 -0400

    feat: project 88%

--- RTS/src/App.tsx
+++ RTS/src/App.tsx
@@ -4,19 +4,26 @@ import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import { DataContextProvider } from "./context/DataContext";
 import Vendas from "./pages/Vendas";
+import { Venda } from "./pages/Venda";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const App = () => {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <SideNav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <SideNav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 };
 
--- RTS/src/components/SideNav.tsx
+++ RTS/src/components/SideNav.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import resumo from "../assets/icons/resumo.svg";
+import configuracoes from "../assets/icons/configuracoes.svg";
+import contato from "../assets/icons/contato.svg";
+import sair from "../assets/icons/sair.svg";
+import vendas from "../assets/icons/vendas.svg";
+import webhooks from "../assets/icons/webhooks.svg";
+import { FintechSVG } from "../assets/icons/FintechSVG";
+import { NavLink } from "react-router";
 
 const SideNav = () => {
-  return <div>SideNav</div>;
+  return (
+    <aside>
+      <nav className="sidenav box bg-3">
+        <FintechSVG title="Fintech" />
+        <ul>
+          <li>
+            <span>
+              <img src={resumo} />
+            </span>
+            <NavLink to="/">Resumo</NavLink>
+          </li>
+          <li>
+            <span>
+              <img src={vendas} />
+            </span>
+            <NavLink to="/vendas">Vendas</NavLink>
+          </li>
+          <li>
+            <span>
+              <img src={webhooks} />
+            </span>
+            <a>Webhooks</a>
+          </li>
+          <li>
+            <span>
+              <img src={configuracoes} />
+            </span>
+            <a>Configuracoes</a>
+          </li>
+          <li>
+            <span>
+              <img src={contato} />
+            </span>
+            <a>Contato</a>
+          </li>
+          <li>
+            <span>
+              <img src={sair} />
+            </span>
+            <a>Sair</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 };
 
 export default SideNav;
